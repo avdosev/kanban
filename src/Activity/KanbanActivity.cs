918957e422e433f583d1227d5d3000567fae1fce
kyplyk <avdosev1@yandex.ru>
2019-12-24 22:47:24 +0400

    small refactor

--- src/Activity/KanbanActivity.cs
+++ src/Activity/KanbanActivity.cs
@@ -5,10 +5,12 @@ using System.Text;
 
 using Android.App;
 using Android.OS;
+using Android.Content;
 using Android.Runtime;
 using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Support.V7.Widget;
+using Android.Support.V4.App;
 using Android.Views;
 
 namespace src {
@@ -24,8 +26,64 @@ namespace src {
 
             var tabbar = FindViewById<TabLayout>(Resource.Id.tab_layout);
 
+            tabbar.AddTab(tabbar.NewTab().SetText("test1"));
+            tabbar.AddTab(tabbar.NewTab().SetText("test2"));
+            tabbar.AddTab(tabbar.NewTab().SetText("test3"));
+
+            var viewPager = FindViewById<Android.Support.V4.View.ViewPager>(Resource.Id.viewpager);
+            viewPager.Adapter = new ColumnPagerAdapter(SupportFragmentManager);
+
+            tabbar.SetupWithViewPager(viewPager);
+        }
+    }
+
+    public class ColumnPageFragment : Android.Support.V4.App.Fragment {
+        private const string ARG_PAGE = "ARG_PAGE";
+
+        private int mPage;
+
+        public static ColumnPageFragment newInstance(int page) {
+            Bundle args = new Bundle();
+            args.PutInt(ARG_PAGE, page);
+            ColumnPageFragment fragment = new ColumnPageFragment();
+            fragment.Arguments = args;
+            return fragment;
+        }
+
+        override public void OnCreate(Bundle savedInstanceState) {
+            base.OnCreate(savedInstanceState);
+            if (Arguments != null) {
+                mPage = Arguments.GetInt(ARG_PAGE);
+            }
+        }
+
+        override public View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View view = inflater.Inflate(Resource.Layout.kanban_content, container, false);
+            // Making recicler view
+            return view;
+        }
+    }
+
+   
+
+    public class ColumnPagerAdapter : FragmentPagerAdapter {
+        const int PAGE_COUNT = 3;
+        private string[] tabTitles = new string[] { "Tab1", "Tab2", "Tab3" };
+        Context context;
+        public override int Count {
+            get { return PAGE_COUNT; }
+        }
+
+        public ColumnPagerAdapter(Android.Support.V4.App.FragmentManager fm) : base(fm) {
             
-            
+        }
+
+        public override Java.Lang.ICharSequence GetPageTitleFormatted(int position) {
+            return new Java.Lang.String(tabTitles[position]);
+        }
+
+        public override Android.Support.V4.App.Fragment GetItem(int position) {
+            return ColumnPageFragment.newInstance(position);
         }
     }
 }
